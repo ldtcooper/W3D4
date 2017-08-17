fce5bc7fd758c95064fdcec5bed355ed9d71a9ad
App Academy Student <student@appacademy.io>
2017-08-17 16:14:22 -0700

    added basic associations to models

--- app/models/response.rb
+++ app/models/response.rb
@@ -10,4 +10,14 @@
 #
 
 class Response < ApplicationRecord
+
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_id,
+    class_name: :AnswerChoice
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@
 #
 
 class User < ApplicationRecord
+
+  has_many :authored_polls,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Poll
+
+    has_many :responses,
+      primary_key: :id,
+      foreign_key: :user_id,
+      class_name: :response
 end
